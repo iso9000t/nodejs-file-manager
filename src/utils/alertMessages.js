8936c956979dfc9923d84d8c5939365ea5a76537
iso9000t <sasha.gr81@gmail.com>
2024-02-05 18:16:45 -0300

    refactor: delete empty lines

--- src/utils/alertMessages.js
+++ src/utils/alertMessages.js
@@ -2,15 +2,9 @@ export const welcomeMessage = (user) => {
   console.log(`Welcome to the File Manager, ${user}!`);
 };
 
-
-
-
 export const goodbyeMessage = (user) => {
   console.log(`Thank you for using File Manager, ${user}, goodbye!`);
 };
-
-
-
 
 export const showCurrentDirectory = (directory) => {
   console.log(`You are currently in ${directory}`);
