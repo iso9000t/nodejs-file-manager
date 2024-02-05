11eafa652b52671ea0b58d5b0784d5a6866333c5
iso9000t <sasha.gr81@gmail.com>
2024-02-05 16:37:32 -0300

    feat add index.js, app.js logic, and welcome utils

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('File Manager is starting up...');
+import { FileManagerApp } from './app.js';
+import { homedir } from 'node:os';
+import welcomeUser from './utils/welcome.js';
+
+welcomeUser();
+
+const fileManager = new FileManagerApp(homedir());
+await fileManager.init();
